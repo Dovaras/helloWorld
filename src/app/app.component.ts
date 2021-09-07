e5c67a4a7d4995b132595e2214746f6110ecd53b
Arūnas Dovainis <arunas.dovainis@gmail.com>
2021-09-07 14:14:13 +0300

    Interpoliacija

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'xyz',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './app2.css']
 })
 export class AppComponent {
   title = 'helloWorld';
+  subtitle = 'This is an amazing application';
+  firstNumber = 5;
+  secondNumber = 10;
+  counter = 0;
+
+  clickHandlerPlus() {
+    this.counter++;
+  }
+  clickHandlerMinus() {
+    this.counter--;
+  }
 }
---
+++ src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `<p>Header works!</p>`,
+  styles: ['']
+})
+export class HeaderComponent {
+}
